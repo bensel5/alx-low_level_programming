24f153a1ad22765a572e75431f322997e5d9e5c6
bensel5 <benedicta.selasi8@gmail.com>
2023-04-25 03:51:34 +0100

    advanced error

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,6 +12,7 @@ for (i = 0; i < 99; i++)
 for (j = 0; j < 99; j++)
 {
 if (i < j && i != j)
+{
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
 putchar(' ');
@@ -21,7 +22,7 @@ if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
-}}}
+}}}}
 putchar('\n');
 return (0);
 }
